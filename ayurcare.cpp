687d7ad52f326e3de205e63aa9cb189d05def2e9
Maestro-alpha <kchirag.gautam.30@gmail.com>
2026-04-17 16:59:24 +0530

    First try

---
+++ ayurcare.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+class medicine
+{
+    char name[50];
+    int expiry;
+    int stock;
+    float price;
+};
+
+class patient
+{
+    int patientId;
+    char name[100];
+    char address[300];
+    long long int phoneNo;
+};
+
+int main()
+{
+    return 0;
+}
